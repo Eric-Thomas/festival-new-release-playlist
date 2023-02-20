e922cbce3709157bec17750cc29277ae34a526d3
Eric Thomas <ethomasosu@gmail.com>
2023-02-20 16:38:29 -0500

    add metacritic scraper

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from selenium.webdriver.chrome.options import Options
 
 from scraper import Scraper
 
+
 def handler(event, context):
     main()
+
 
 def main():
     try:
         browser = get_chrome_browser()
         scraper = Scraper(browser)
+        releases = scraper.scrape_new_releases()
     finally:
         browser.quit()
 
@@ -20,17 +23,17 @@ def get_chrome_browser() -> webdriver.Chrome:
     if (
         os.environ.get("RUNTIME_ENV") != None
         and os.environ.get("RUNTIME_ENV").lower() == "prod"
-    ): # Prod driver is packed into lambda layer
+    ):  # Prod driver is packed into lambda layer
         options = Options()
-        options.binary_location = '/opt/headless-chromium'
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--single-process')
-        options.add_argument('--disable-dev-shm-usage')
-        return webdriver.Chrome('/opt/chromedriver',options=options)
+        options.binary_location = "/opt/headless-chromium"
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--single-process")
+        options.add_argument("--disable-dev-shm-usage")
+        return webdriver.Chrome("/opt/chromedriver", options=options)
     else:
         # Local chrome dirver for development. Should be named 'chromedriver'
-        return webdriver.Chrome(os.environ.get("CHROMEDRIVER_LOCATION"))
+        return webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"))
 
 
 if __name__ == "__main__":
--- scraper.py
+++ scraper.py
@@ -1,5 +1,79 @@
+import re
+import time
+from typing import List
+
 from selenium.webdriver import Chrome
+from bs4 import BeautifulSoup
+
 
 class Scraper:
+
+    METACRITIC_NEW_RELEASES_URL = (
+        "https://www.metacritic.com/browse/albums/release-date/new-releases/date"
+    )
+    METACRITIC_BASE_URL = "https://www.metacritic.com"
+
     def __init__(self, browser: Chrome):
         self.browser = browser
+
+    def scrape_new_releases(self):
+        releases = []
+        current_url = self.METACRITIC_NEW_RELEASES_URL
+        while current_url:
+            releases.append(self._scrape_page(current_url))
+            next_url = self._get_next_url(current_url)
+            current_url = next_url
+
+        return releases
+
+    def _get_next_url(self, url):
+        self.browser.get(url)
+        time.sleep(2)
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
+        next = soup.find("a", rel="next")
+        if next:
+            return f"{self.METACRITIC_BASE_URL}{next.attrs.get('href')}"
+        return None
+
+    def _scrape_page(self, url):
+        print(f"Scraping page {url}")
+        self.browser.get(url)
+        time.sleep(2)
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
+        container_divs = soup.find_all("div", class_="browse_list_wrapper")
+        releases = []
+        for container_div in container_divs:
+            releases.append(self._get_releases(container_div))
+
+        return releases
+
+    def _get_releases(self, container_div) -> List:
+        releases = []
+        release_rows = container_div.find_all("tr")
+        for release_row in release_rows:
+            release_info = self._get_release_info(release_row)
+            if release_info:
+                releases.append(release_info)
+
+        return releases
+
+    def _get_release_info(self, release_row):
+        # Ignore spacer rows
+        if release_row.attrs.get("class") and "spacer" in release_row.attrs["class"]:
+            return
+
+        release_info = {}
+
+        album_name = release_row.find("h3").text
+        release_info["album_name"] = album_name
+
+        artist_div = release_row.find("div", class_="artist")
+        artist_name = artist_div.text.strip().split()[1]
+        release_info["artist_name"] = artist_name
+
+        release_date = release_row.find("span").text.strip()
+        release_info["release_date"] = release_date
+
+        print(release_info)
+
+        return release_info
